4288295b3acabe41509c15fcf70e923e745be912
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-16 19:43:36 +0530

    Time: 2 ms, Space: 43 MB - AlgoVault

--- leetcode/hard/996-number-of-squareful-arrays/solution.java
+++ leetcode/hard/996-number-of-squareful-arrays/solution.java
@@ -1,48 +1,63 @@
 class Solution {
-    public boolean perfectSquare(int n){
-        if(n<0) return false; 
-        int sqrt = (int)Math.sqrt(n); 
-        return (sqrt * sqrt == n);
+
+    public boolean perfectSquare(int n) {
+        if (n < 0) return false;
+
+        int sqrt = (int) Math.sqrt(n);
+        return sqrt * sqrt == n;
     }
-    public int f(int i , int prev , int mask , int nums[]){
-        int n = nums.length ; 
-        if(mask ==(1<< n )-1) {
-            return 1; 
+
+    public int f(int i, int prev, int mask, int nums[], Set<Integer> used) {
+
+        int n = nums.length;
+
+        // All elements taken
+        if (mask == (1 << n) - 1) {
+            return 1;
         }
-        if(i == n) return 0 ;
 
-        if (i > 0 &&
-            nums[i] == nums[i - 1] &&
-            (mask & (1 << (i - 1))) == 0) {
-
-            return f(i + 1, prev, mask, nums);
-        } 
-
-        int take = 0 ; 
-        int skip = 0 ; 
-        if((mask & (1 << i)) != 0 ){
-            return f(i+1 , prev , mask , nums); 
+        // Reached end
+        if (i == n) {
+            return 0;
         }
-        if(prev == -1){
-            int newMask  = mask | (1 <<i); // mask it
-            take = f(0 , i , newMask , nums );
+
+        // Already taken
+        if ((mask & (1 << i)) != 0) {
+            return f(i + 1, prev, mask, nums, used);
         }
-        else if(perfectSquare(nums[i] + nums[prev])){ //not already taken 
-            int newMask = mask | (1 << i); // mask it 
-            take = f(0 , i , newMask , nums );
+
+        // Duplicate value at this recursion level
+        if (used.contains(nums[i])) {
+            return f(i + 1, prev, mask, nums, used);
         }
-        skip = f(i+1 , prev , mask , nums);
 
-        return take + skip ; 
+        // Mark this value as tried at this level
+        used.add(nums[i]);
 
+        int take = 0;
+        int skip = 0;
 
+        // Take
+        if (prev == -1 || perfectSquare(nums[i] + nums[prev])) {
+
+            int newMask = mask | (1 << i);
+
+            // New recursion level -> new Set
+            take = f(0, i, newMask, nums, new HashSet<>());
+        }
+
+        // Skip
+        // IMPORTANT: don't remove nums[i] from used.
+        // We are still at the same recursion level.
+        skip = f(i + 1, prev, mask, nums, used);
+
+        return take + skip;
     }
+
     public int numSquarefulPerms(int[] nums) {
-        // we can do is take take 1 + 17 = 18is it no take skip 1 + 8 = 9 yes now move to 0 and see all the remaining one for that we can mask the ones we have already taken 
 
-        int mask = 0;
-        Arrays.sort(nums); 
+        Arrays.sort(nums);
 
-        return f(0 , -1 ,  mask  , nums);
+        return f(0, -1, 0, nums, new HashSet<>());
     }
 }
