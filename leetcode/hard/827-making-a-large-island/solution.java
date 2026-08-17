c6d3aa4d432ef6959032bd551a4bc50962aceeca
Som <149756531+Somnath0707@users.noreply.github.com>
2026-08-17 10:08:11 +0530

    Time: 160 ms, Space: 158 MB - AlgoVault

--- leetcode/hard/827-making-a-large-island/solution.java
+++ leetcode/hard/827-making-a-large-island/solution.java
@@ -72,7 +72,7 @@ class Solution {
         int id = 2; 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] != 0 && nums[i][j] == -1){
+                if(grid[i][j] != 0){
                     ans = Math.max(ans , bfs( grid , i , j , false , id));
                     id++; 
                 }
